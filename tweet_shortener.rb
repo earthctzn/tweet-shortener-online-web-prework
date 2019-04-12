9f206b0099e4abbb627dee2cc05c1f96ee90ef37
Caleb Einarr <earthctzn1@gmail.com>
2019-04-12 01:20:53 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,38 @@
-# Write your code here.
+def dictionary 
+  wrds = {
+  "hello" => "hi", 
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+}# Write your code here.
+end
+
+def word_substituter(str)
+  tweet = str.split(" ").map do |string|
+    if dictionary.keys.include?(string.downcase)
+      string = dictionary[string.downcase]
+    else
+      string
+    end
+    end
+    tweet.join(" ")
+end
+
+def bulk_tweet_shortener(twts)
+  twts.each {|blah|puts word_substituter(blah)}
+end
+
+def selective_tweet_shortener(twts)
+  if twts.lenght >= [130..140]
+    word_substituter(twts)
+  else
+    twts
+  end
+end
+
